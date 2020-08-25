67635b4390386ad92963f3d28acdc36520c71a9d
Carlos Martinez <edu6jas@gmail.com>
2020-08-25 19:50:49 -0400

    fix typos

--- js/utils/get_id.js
+++ js/utils/get_id.js
@@ -1,5 +1,3 @@
-import { get } from "http";
-
 const getNode = (id) => document.getElementById(id);
 
 export default getNode;
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   // indica la salida de los archivos empaquetados en uno solo
   output: {
-    path: path.resolve(__dirname, "buid/"),
+    path: path.resolve(__dirname, "build/"),
     filename: "app.js",
   },
 
