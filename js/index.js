27f3a52fd52e2a44391cf11e592a3c920ae664a3
libardofox <libardojesusrengifo129@gmail.com>
2020-08-25 19:37:42 -0430

    loading...

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 import { getInputsRangeToPutToolTips } from "./input-range.js";
+import getNode from "./utils/get_id";
 import "./modal";
 
-
 window.addEventListener("load", (e) => {
+  getNode("loader").style.display = "none";
   getInputsRangeToPutToolTips();
 });
